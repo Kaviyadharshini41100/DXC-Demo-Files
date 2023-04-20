6bb455d364e2b22ad8783411d86c3d236ea237ac
S <kaviyadharshini.s@dxc.com>
2023-04-20 12:34:47 +0530

    Second Commit

---
+++ Large num/Large num/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Large_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter num1");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter num2");
+            num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter num3");
+            num3 = Convert.ToInt16(Console.ReadLine());
+            if (((num1 > num2) && (num1 > num3)))
+            {
+                Console.WriteLine("num1 is Greater");
+            }
+            else if (((num2 > num1) && (num2 > num3)))
+            {
+                Console.WriteLine("num2 is Greater");
+            }
+            else
+            {
+                Console.WriteLine("num3 is Greater");
+            }
+        }
+    }
+}
---
+++ Vowel/Vowel/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Vowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter character");
+            char ap= Convert.ToChar(Console.ReadLine());
+            if(ap == 'A' || ap == 'a' || ap == 'E' || ap == 'e' || ap == 'I' || ap == 'i' || ap == 'O' || ap == 'o' || ap == 'U' || ap == 'u')
+            {
+                Console.WriteLine("Its an  vowel");
+            }
+            else
+            {
+                Console.WriteLine("Its  not an  vowel");
+            }
+        }
+    }
+}
