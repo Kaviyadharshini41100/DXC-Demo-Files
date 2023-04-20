a46a6659064d20280cfbd8952f1e8f363136b2e9
S <kaviyadharshini.s@dxc.com>
2023-04-20 14:49:51 +0530

    Third Commit

---
+++ Student_Details/Student_Details/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Student_Details
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Student name");
+            String StudentName = Console.ReadLine();
+            Console.WriteLine("Enter Student ID");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Mark1:");
+            int Mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Mark2:");
+            int Mark2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Mark3:");
+            int Mark3 = Convert.ToInt32(Console.ReadLine());
+
+            double Total = Mark1 + Mark2 + Mark3;
+            double Avg = Total / 3;
+            double Per = (Total / 300) * 100;
+            Console.WriteLine("Total Marks are" + Total);
+            Console.WriteLine("Percentage is" + Per);
+                if (Per >= 90 && Per<= 100)
+                {
+                    Console.WriteLine("A Grade");
+                }
+                else if (Per >= 80 && Per <= 89)
+                {
+                    Console.WriteLine("B Greade");
+                }
+                else if (Per >= 60 && Per <= 79)
+                {
+                    Console.WriteLine("C Greade");
+                }
+                else if (Per >= 50 && Per <= 59)
+                {
+                    Console.WriteLine("D Greade");
+                }
+                else
+                {
+                    Console.WriteLine("E Greade");
+
+                }
+                if(Per >= 50)
+                 {
+                   Console.WriteLine("Result is Pass");
+                }
+                else
+                {
+                   Console.WriteLine("Result is Fail");
+                }
+            
+
+            
+
+           
+            
+
+
+
+
+
+
+
+        }
+    }
+}
