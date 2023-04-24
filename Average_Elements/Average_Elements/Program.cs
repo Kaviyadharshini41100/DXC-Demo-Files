9f7fc38ae7794506cd29cd6bb3d4438e927495c1
S <kaviyadharshini.s@dxc.com>
2023-04-24 14:11:49 +0530

    updated

--- Average_Elements/Average_Elements/Program.cs
+++ Average_Elements/Average_Elements/Program.cs
@@ -4,15 +4,33 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = {10,10,10,10,10,10,10,10,10,10 };
-            int sum = 0;
+            int[] arr = new int[10];
+
+            Console.WriteLine("Enter 10 elements in an array");
+
             for (int i = 0; i < arr.Length; i++)
             {
-                sum += arr[i]; 
+
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+
             }
-            double average = (double)sum / arr.Length;
-                                                       
-            Console.WriteLine("Average of elements: " + average);
+
+            double sum = 0;
+
+            double average;
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+
+                sum += arr[j];
+
+            }
+
+            average = sum / arr.Length;
+
+            Console.WriteLine("Average of given elements:" + average);
+
+            
         }
     }
 }
