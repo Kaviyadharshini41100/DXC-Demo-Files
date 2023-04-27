f3ed08f34864c9768edf0e18c62cca891fb295ca
S <kaviyadharshini.s@dxc.com>
2023-04-27 17:39:44 +0530

    Initial_Commit

---
+++ InheritanceDemo/InheritanceDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InheritanceDemo
+{
+    internal class Calculator
+    {
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a,int b)
+        {
+            return a - b;
+        }
+    }
+    class SubCalculator : Calculator
+    {
+        public int Square(int x)
+        {
+            return x * x;
+        }
+        public void Message()
+        {
+            Console.WriteLine("This is new one");
+        }
+    }
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            SubCalculator calc = new SubCalculator();
+            calc.Message();
+        }
+    }
+}
---
+++ PolymorphismDemo/PolymorphismDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PolymorphismDemo
+{
+     class Program
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("This is new one");
+        }
+    }
+    class Student : Program
+    {
+        public override void show()
+        {
+            Console.WriteLine("This is Second method");
+        }
+    }
+    internal class detail
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Student();
+            p.show();
+        }
+    }
+      
+}
