66305079df545c4dce487128ea71bdd77f81cd99
S <kaviyadharshini.s@dxc.com>
2023-04-28 13:07:20 +0530

    Initial_Commit

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Interface
+{
+    interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int x);
+    }
+    class Car: IVehicle
+    {
+        public int gasoline;
+        public Car(int startinggasoline)
+        {
+            gasoline = startinggasoline;
+        }
+        public void Drive()
+        {
+            if(gasoline>0)
+            {
+                Console.WriteLine("The car is driving");
+            }
+        }
+        public bool Refuel(int amount)
+        {
+            if(amount<0)
+            {
+
+                Console.WriteLine("Invaild amount");
+                return false;
+            }
+            gasoline += amount;
+
+            Console.WriteLine("gasoline refueled.current level:"+gasoline);
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter amount of gasoline to refuel");
+            int amount = int.Parse(Console.ReadLine());
+            Car car = new Car(0);
+            car.Refuel(amount);
+            car.Drive();
+
+        }
+    }
+
+}
