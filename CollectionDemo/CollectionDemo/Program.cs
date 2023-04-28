7106e964631af90ccd4a817819f073ee5c6b8466
S <kaviyadharshini.s@dxc.com>
2023-04-28 22:31:58 +0530

    Initial_Commit

---
+++ CollectionDemo/CollectionDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(40);
+            list.Add("Hello");
+
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+            list.Insert(3, "World");
+            foreach(var i in list)
+            {
+               Console.WriteLine(i);
+           }
+            Console.WriteLine(list[2]);
+            Console.WriteLine(list.Count);
+        }
+        
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int n1, n2, n3;
+            int day;
+            Console.WriteLine("Enter first num");
+            n1=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second num");
+            n2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter third num");
+            n3 = Convert.ToInt16(Console.ReadLine());
+            if((n1>n2) && (n1>n3))
+            {
+                Console.WriteLine("Number 1 is greater");
+            }
+            else if((n2>n1) && (n2>n3))
+            {
+                Console.WriteLine("Number 2 is greater");
+            }
+            else
+            {
+                Console.WriteLine("Number 3 is greater");
+            }
+            Console.WriteLine("Program Executed successfully");
+            Console.WriteLine();
+            day = 5;
+            switch(day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wednesdday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Thursday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Friday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Saturday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid input");
+                        break;
+                    }
+            }
+            Console.WriteLine("Switch case Program");
+            
+
+        }
+        
+    }
+}
