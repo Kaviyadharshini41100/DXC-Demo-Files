558b2a2f4b12d5ff06acae671951bfab9ef5af18
S <kaviyadharshini.s@dxc.com>
2023-05-02 17:40:17 +0530

    Initial_Commit

---
+++ ExceptionDemo/ExceptionDemo/InValidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    internal class InValidException : Exception
+    {
+        public InValidException() { }
+        public InValidException(String message) : base (message) { }
+    }
+}
---
+++ ExceptionDemo/ExceptionDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            
+            try
+            {
+                Console.WriteLine("Enter number 1");
+                a = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter number 2");
+                b = Convert.ToInt16(Console.ReadLine());
+                if (a < b)
+                {
+                    throw new InValidException("Value of first number cannot be less than second number");
+                }
+                c = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide any number by zero!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only numbers are allowed");
+            }
+            catch (InValidException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            finally
+            {
+                Console.WriteLine($"The result is {c}");
+                Console.WriteLine("Program End.");
+            }
+        }
+           
+
+    }
+    
+}
